42319d0d7b16db1ca75451400ef8f68353906f1d
lemonkang <1084920778@qq.com>
2021-12-19 15:48:36 +0800

    统一日志管理

--- common/service_base/src/main/java/com/atguigu/servicebase/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/atguigu/servicebase/GlobalExceptionHandler.java
@@ -3,15 +3,18 @@ package com.atguigu.servicebase;
 
 
 import com.atguigu.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ResponseBody
     @ExceptionHandler(Exception.class)
     R error(Exception e){
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.fail();
     }
